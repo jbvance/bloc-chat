ab3675f07767a69e654b1245e2da1d5f77770ec4
Jason Vance <jbvance@gmail.com>
2017-01-04 21:12:45 -0600

    Write minor code formatting revision

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,14 +1,14 @@
-(function() {
-      function HomeCtrl($scope, Room) {
+(function () {
+    function HomeCtrl($scope, Room) {
         //Set the page heading
         this.heading = "Bloc Chat";
           
-       //Get a list of all the chat rooms
-       $scope.chatRooms = Room.all;
+        //Get a list of all the chat rooms
+        $scope.chatRooms = Room.all;
                            
-     }
+    }
  
-     angular
+    angular
          .module('blocChat')
          .controller('HomeCtrl', ['$scope', 'Room', HomeCtrl]);
- })();
+})();
